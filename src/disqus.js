3e8c04ab450e1036a1363c6b06aebd179ef959a4
SukkaW <isukkaw@gmail.com>
2019-11-04 13:13:55 +0800

    init: bring up disqusjs

--- src/disqus.js
+++ src/disqus.js
@@ -1,6 +1,6 @@
 function DisqusJS(config) {
     /*
-     * A polyfill of Object.asign
+     * Object.asign 的 Polyfill
      */
     function _extends() {
         _extends = Object.assign || function (target) {
@@ -17,15 +17,24 @@ function DisqusJS(config) {
     }
 
     /*
-     * A polyfill of Array.isArray
+     * Array.isArray 的 Polyfill
      */
     if (!Array.isArray) Array.isArray = (arg) => Object.prototype.toString.call(arg) === '[object Array]';
 
-
     ((window, document, localStorage, config) => {
+        const $$ = (id) => document.getElementById(id);
+
+        /*
+         * msg(text)
+         * 打印 Tip
+         */
+        const msg = (str) => {
+            const msgEl = $$('dsqjs-msg');
+            if (msgEl) msgEl.innerHTML = str;
+        }
         /*
          * get(url)
-         * A wrapper of fetch GET method.
+         * 一个 Fetch 的包装
          */
         const get = (url) => fetch(url, { method: 'GET' })
             .then((resp) => Promise.all([resp.ok, resp.status, resp.json(), resp.headers]))
@@ -47,7 +56,7 @@ function DisqusJS(config) {
 
         /*
          * setLS(key, value)
-         * store value inside localStorage
+         * localStorage 存储
          */
         const setLS = (key, value) => {
             try {
@@ -57,22 +66,117 @@ function DisqusJS(config) {
 
         /*
          * formatDate(date)
-         * Format a input date to ${y}-${m}-${d} ${h}:${minute} format
+         * 将传入的 UTC 日期格式化为 ${y}-${m}-${d} ${h}:${minute} format
          */
         const formatDate = (date) => {
             // 不足两位补 0
-            let x = (input) => (input < 10) ? `0${input}` : input;
+            const x = (input) => (input < 10) ? `0${input}` : input;
             // 将传入的 date 转化为时间戳
             date = Date.parse(new Date(date));
 
             // Disqus API 返回的是 UTC 时间，所以在时间戳上加 28800000 毫秒补上 8 小时时差
             date = new Date(date + 8 * 60 * 60 * 1000);
-            let y = date.getFullYear();
-            let m = x(date.getMonth() + 1);
-            let d = x(date.getDate());
-            let h = x(date.getHours());
-            let minute = x(date.getMinutes());
+            const y = date.getFullYear();
+            const m = x(date.getMonth() + 1);
+            const d = x(date.getDate());
+            const h = x(date.getHours());
+            const minute = x(date.getMinutes());
             return `${y}-${m}-${d} ${h}:${minute}`;
+        }
+
+        /*
+         * loadDisqus() - 加载/重载 Disqus
+         */
+        function loadDisqus() {
+            if (window.DISQUS) {
+                window.DISQUS.reset({
+                    reload: true,
+                    config: () => {
+                        this.page.identifier = disqusjs.config.identifier;
+                        this.page.url = disqusjs.config.url;
+                        this.page.title = disqusjs.config.title;
+                    }
+                });
+            } else {
+                msg(`评论完整模式加载中...如果长时间无法加载，请针对 disq.us | disquscdn.com | disqus.com 启用代理，或使用 <a id="dsqjs-btn-force-dsqjs" class="dsqjs-msg-btn">评论基础模式</a>`);
+                $$('dsqjs-btn-force-dsqjs').addEventListener('click', useDsqjs);
+
+                const s = document.createElement('script');
+                s.src = `https://${disqusjs.config.shortname}.disqus.com/embed.js`;
+                s.setAttribute('data-timestamp', +new Date());
+                (document.head || document.body).appendChild(s);
+            }
+        }
+
+        /*
+         * loadDisqus() - 加载 DisqusJS
+         */
+        function loadDsqjs() {
+            msg(`评论基础模式加载中。如需完整体验请针对 disq.us | disquscdn.com | disqus.com 启用代理并 <a id="dsqjs-btn-reload-disqus" class="dsqjs-msg-btn">尝试完整 Disqus 模式</a> | <a id="dsqjs-btn-force-disqus" class="dsqjs-msg-btn">强制完整 Disqus 模式</a>。`);
+            $$('dsqjs-btn-reload-disqus').addEventListener('click', checkDisqus);
+            $$('dsqjs-btn-force-disqus').addEventListener('click', useDisqus);
+        }
+
+        /*
+         * checkDisqus()
+         * 检查 Disqus 的可访问性
+         */
+        function checkDisqus() {
+            $$('disqus_thread').innerHTML = `<div id="dsqjs"><div id="dsqjs-msg"></div></div>`;
+            msg(`正在检查您是否能够访问 Disqus...`);
+
+            const domain = ['disqus.com', `${disqusjs.config.shortname}.disqus.com`];
+            let test = 0, success = 0;
+
+            const checker = () => {
+                if (domain.length === test && test === success) {
+                    useDisqus();
+                } else if (domain.length === test) {
+                    useDsqjs();
+                }
+            }
+
+            const runcheck = (domain) => {
+                let img = new Image;
+                const timeout = setTimeout(() => {
+                    img.onerror = img.onload = null;
+                    test++;
+                    checker();
+                }, 3000);
+
+                img.onerror = () => {
+                    clearTimeout(timeout);
+                    test++;
+                    checker();
+                }
+
+                img.onload = () => {
+                    clearTimeout(timeout);
+                    test++;
+                    success++;
+                    checker();
+                }
+
+                img.src = `https://${domain}/favicon.ico?${+(new Date)}`
+            }
+
+            for (let i of domain) {
+                runcheck(i);
+            }
+        }
+
+        /*
+         * useDsqjs() - 强制使用 DisqusJS
+         * useDisqus() - 强制使用 Disqus
+         */
+        function useDsqjs() {
+            setLS('dsqjs_mode', 'dsqjs')
+            loadDsqjs()
+        }
+
+        function useDisqus() {
+            setLS('dsqjs_mode', 'disqus')
+            loadDisqus()
         }
 
         let disqusjs = {};
@@ -85,7 +189,7 @@ function DisqusJS(config) {
             nesting: parseInt(config.nesting) || 4
         }, config);
 
-        const apikey = () => (Array.isArray(disqusjs.config.apikey)) ? disqusjs.config.apikey[Math.floor(Math.random() * disqusjs.config.apikey.length)] : disqusjs.config.apikey;
+        let apikey = () => (Array.isArray(disqusjs.config.apikey)) ? disqusjs.config.apikey[Math.floor(Math.random() * disqusjs.config.apikey.length)] : disqusjs.config.apikey;
 
         window.disqus_config = function () {
             this.page.url = disqusjs.config.url;
@@ -93,15 +197,24 @@ function DisqusJS(config) {
             this.page.title = disqusjs.config.title;
         };
 
-        if (!disqusjs.sortType) {
-            setLS('disqus.sort', 'desc');
-            disqusjs.sortType = 'desc';
-        }
-
         disqusjs.mode = localStorage.getItem('dsqjs_mode');
         disqusjs.sortType = localStorage.getItem('dsqjs_sort') || localStorage.getItem('disqus.sort');
 
+        $$('disqus_thread').innerHTML = `<div id="dsqjs"><div id="dsqjs-msg"></div></div>`
+
+        if (!disqusjs.sortType) {
+            setLS('dsqjs_sort', 'desc');
+            disqusjs.sortType = 'desc';
+        }
+        if (disqusjs.mode === 'disqus') {
+            loadDisqus();
+        } else if (disqusjs.mode === 'dsqjs') {
+            loadDsqjs();
+        } else {
+            checkDisqus();
+        }
+
     })(window, document, localStorage, config);
 }
-// attempt to export for CommonJS
+// CommonJS
 try { module.exports = DisqusJS; } catch (e) { }
